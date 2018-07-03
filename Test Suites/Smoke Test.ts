7fd8cce4518011b964c1bbf788b39da9a2ca52a4
Trong Bui <>
2018-07-03 14:36:53 +0700

    Add tutorial's materials

---
+++ Test Suites/Smoke Test.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smoke Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c104d40b-6f5c-473e-b3c7-e680da52f0b3</testSuiteGuid>
+</TestSuiteEntity>
